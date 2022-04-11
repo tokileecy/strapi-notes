9fc9b835398ffb0c95c2ff611bc21b0a55f35fe5
tokileecy <tokileecy@gmail.com>
2022-04-11 17:02:00 +0000

    feat: add tag and category

--- apps/strapi-app/src/api/category/routes/category.js
+++ apps/strapi-app/src/api/category/routes/category.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * blog router.
+ * category router.
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::blog.blog');
+module.exports = createCoreRouter('api::category.category');
--- apps/strapi-app/src/api/tag/services/tag.js
+++ apps/strapi-app/src/api/tag/services/tag.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * blog service.
+ * tag service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::blog.blog');
+module.exports = createCoreService('api::tag.tag');
