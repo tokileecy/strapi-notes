5954dbfeefd8afb66535d64f4919b5632d06d3b3
tokileecy <tokileecy@gmail.com>
2022-05-02 13:03:06 +0000

    fix: "missing apiToken.salt" warning
    https://github.com/strapi/strapi/issues/13126

--- apps/strapi-app/config/admin.js
+++ apps/strapi-app/config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'ffjwq7wqe9qw9dv2221215707b4e3e7'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '0bb4d6fbe9ffe647b5ef199ed63eef46'),
   },
