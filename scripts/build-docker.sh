1939b853331daa23723069babe8555c0626db6ff
tokileecy <tokileecy@gmail.com>
2022-07-04 08:48:57 +0000

    chore: add production docker

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,3 +4,4 @@ BASEDIR=$(dirname $(realpath "$0"))
 ROOT_DIR=$(cd $BASEDIR; cd ..; pwd)
 
 docker build -t strapi-notes-strapi-app -f docker/strapi-app/Dockerfile .
+docker build -t strapi-notes-next-app -f docker/next-app/Dockerfile .
