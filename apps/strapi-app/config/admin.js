e8f5baac212ae3ff34f5f8161b5d2cbffdece3d1
tokileecy <tokileecy@gmail.com>
2023-03-07 07:19:15 +0000

    chore: upgrade strapi version to v4.7.0

--- apps/strapi-app/config/admin.js
+++ apps/strapi-app/config/admin.js
@@ -3,6 +3,11 @@ module.exports = ({ env }) => ({
     salt: env('API_TOKEN_SALT', 'ffjwq7wqe9qw9dv2221215707b4e3e7'),
   },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '0bb4d6fbe9ffe647b5ef199ed63eef46'),
+    secret: env('ADMIN_JWT_SECRET', 'HMaaIx1CVpGmlS7DZ6kHdQ=='),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'dN0ku0Dh9FfjlXbFDPOOlg=='),
+    },
   },
 });
